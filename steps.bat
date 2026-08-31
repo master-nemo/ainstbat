7c2c508681ae558b61574e30a30e3840730d5c09
vturinsa@gmail.com <vturinsa@gmail.com>
2026-08-31 18:32:55 +0300

    ~

--- steps.bat
+++ steps.bat
@@ -32,7 +32,7 @@ if %errorLevel% == 0 (
     winget install --id chocolatey.chocolatey --silent --accept-source-agreements --accept-package-agreements --disable-interactivity >> "%LOG_FILE%" 2>&1
 ) else (
     echo [!] WinGet not found. Installing Chocolatey natively via PowerShell...
-    powershell -NoProfile -ExecutionPolicy Bypass -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org'))" >> "%LOG_FILE%" 2>&1
+    powershell -NoProfile -ExecutionPolicy Bypass -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org'))" 
 )
 
 echo xxxxxxxxxxxxx
